68c1b53b5e52733a08f15447f6abd88fcda63274
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-03-22 19:21:28 -0300

    feat(router): Add CategoryPage route to Router component

--- src/router.jsx
+++ src/router.jsx
@@ -1,15 +1,17 @@
-import { PageDefault } from "components/PageDefault"
-import { HomePage } from "pages/Home"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { PageDefault } from "components/PageDefault";
+import CategoryPage from "pages/Category";
+import { HomePage } from "pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PageDefault/>}>
+        <Route path="/" element={<PageDefault />}>
           <Route index element={<HomePage />} />
+          <Route path="/category/:id" element={<CategoryPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
