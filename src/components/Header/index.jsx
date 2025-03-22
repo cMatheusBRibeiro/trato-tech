150e3d9c8d25cb6393c0d86cf68b7bae2be0056a
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-03-22 19:21:02 -0300

    feat(header): Update Header component to include description and improve image markup

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,15 @@
-import styles from "./Header.module.scss"
+import styles from "./Header.module.scss";
 
 export const Header = ({ title, description, image, className = "" }) => {
   return (
     <header className={`${styles.header} ${className}`}>
       <div className={styles["header-text"]}>
         <h1>{title}</h1>
+        <h2>{description}</h2>
       </div>
       <div className={styles["header-image"]}>
-        <img
-          alt={description}
-          src={image}
-        />
+        <img alt={description} src={image} />
       </div>
     </header>
   );
-}
+};
