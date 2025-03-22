4b7e0671bf9be988e9c819077eb1ae317e0af25e
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-03-22 19:21:39 -0300

    feat(home): Implement category display with navigation in HomePage component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,13 @@
 import { Header } from "components/Header";
 import relogio from "assets/inicial.png";
 import styles from "./Home.module.scss";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+  const categories = useSelector((state) => state.categories);
+
   return (
     <main>
       <Header
@@ -11,6 +16,22 @@ export const HomePage = () => {
         image={relogio}
         className={styles.header}
       />
+      <div className={styles.categorias}>
+        <div className={styles["categorias-title"]}>
+          <h1>Categorias</h1>
+        </div>
+        <div className={styles["categorias-container"]}>
+          {categories.map((category, index) => (
+            <div
+              key={index}
+              onClick={() => navigate(`/category/${category.id}`)}
+            >
+              <img src={category.thumbnail} alt={category.name} />
+              <h1>{category.name}</h1>
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
   );
 };
